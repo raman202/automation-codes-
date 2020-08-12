09e1f5838ebc7548eba992ba160f5522699b43eb
Raman Paul <rkpaul87@gmail.com>
2020-08-12 16:26:33 -0400

    update 4

---
+++ functions/bin/Yahoosignup.feature
@@ -0,0 +1,31 @@
+#Author: rkpaul87@gmail.com
+Feature: Yahoo Sign Up
+
+  Scenario: validate URL
+    Given user is on the Yahoo sign page
+    When user is able to see correct URl
+    Then user can close browser
+
+  Scenario: validate page title
+    Given user is on the Yahoo sign page
+    When user can see the correct page title
+    Then user can close browser
+
+  Scenario Outline: creat new Yahoo account
+    Given user is on the Yahoo sign page
+    And user can enter the firstname "<Firstname>"
+    And user can enter the lastname "<Lastname>"
+    And user can enter the passward "<Passward>"
+    And user can enter the phone "<PhoneNumber>"
+    When user can select the birth month "<Month>"
+    When user can enter the birth date "<Date>"
+    When user can enter the birth year "<Year>"
+    When user can enter the gender "<Gender>"
+    And user can enter the email "<Email>"
+    Then user can close browser
+
+    Examples: 
+      | Firstname | Lastname | Passward       | PhoneNumber | Month | Date | Year | Gender | Email          |
+      | Raman     | Paul     | TheOne&Only    |  5713839976 | July  |    6 | 1987 | male   | meharpaul1987  |
+      | Monika    | Banger   | MonikaPaul2013 |  5713835390 | May   |    2 | 1990 | female | monikabanger10 |
+      | Mehar     | Paul     | MharPaul2019   |  5713835550 | March |    1 | 2019 | female | meharpaul2019  |
--- functions/src/cucumberSetdefFiles/YahooSignUpStepDeff.java
+++ functions/src/cucumberSetdefFiles/YahooSignUpStepDeff.java
@@ -23,164 +23,94 @@ public class YahooSignUpStepDeff {
 	YahooSignUpWebobjects SignupPage;
 	PandoraSignUpPageObjects SP;
 	functioncode1 obj1;
+		
 	
-	
-			@Given("^that the user is on the Yahoo sign up page$")
-		public void that_the_user_is_on_the_Yahoo_sign_up_page() throws Throwable {
-				
-				System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\testing drivers\\chromedriver.exe");
-				driver = new ChromeDriver();
-				YahooSignUpWebobjects SignupPage;
-				driver.get("https://login.yahoo.com/account/create?specId=yidReg");
-				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);	
-		}
 
-		@When("^user can see the correct pahe title$")
-		public void user_can_see_the_correct_pahe_title() throws Throwable {
-			
-				String ActualPageTitle = driver.getTitle();
-				String ExpactedPageTitle = "Yahoo";
-				Reporter.log("Page Title validated");
-				Assert.assertEquals(ActualPageTitle, ExpactedPageTitle);
-		}
 
-		@When("^user can see the correct URL$")
-		public void user_can_see_the_correct_URL() throws Throwable {
-			
-				String ActiualUrl = driver.getCurrentUrl();
-				String ExpectedUrl = "https://login.yahoo.com/account/create?specId=yidReg";
-				Assert.assertEquals(ActiualUrl, ExpectedUrl);
-				Reporter.log("URL validated");
-		}
+	//You can implement missing steps with the snippets below:
 
-		@Then("^user can creat new Yahoo account$")
-		public void user_can_creat_new_Yahoo_account() throws Throwable {
-			System.out.println("user can now creat a new Yahoo account");
-			
-		}
-
-		@When("^user can enter the firstName \"([^\"]*)\"$")
-		public void user_can_enter_the_firstName(String arg1) throws Throwable {
-			SignupPage.UserFirstName().sendKeys("Raman");
-		}
-
-		@When("^user can enter the lastName \"([^\"]*)\"$")
-		public void user_can_enter_the_lastName(String arg1) throws Throwable {
-			SignupPage.UserLastName().sendKeys(arg1);
-		}
-
-		@When("^user can enter the email \"([^\"]*)\"$")
-		public void user_can_enter_the_email(String arg1) throws Throwable {
-			SignupPage.UserEmail().sendKeys(arg1);
-		}
-
-		@When("^user can enter the password \"([^\"]*)\"$")
-		public void user_can_enter_the_password(String arg1) throws Throwable {
-			SignupPage.UserPassward().sendKeys(arg1);
-		}
-
-		@When("^user can enter the phoneNumber \"([^\"]*)\"$")
-		public void user_can_enter_the_phoneNumber(String arg1) throws Throwable {
-			SignupPage.PhoneNumber().sendKeys(arg1);
-		}
+	@Given("^user is on the Yahoo sign page$")
+	public void user_is_on_the_Yahoo_sign_page() throws Throwable {
 		
-		@When("^user can enter the birthMonth \"([^\"]*)\"$")
-		public void user_can_enter_the_birthMonth(String arg1) throws Throwable {
-			Select Month = new Select(SignupPage.GetMonth());
-			Month.selectByVisibleText("July");
-		}
-
-		@When("^user can emter the birthDate \"([^\"]*)\"$")
-		public void user_can_emter_the_birthDate(String arg1) throws Throwable {
-			SignupPage.GetDate().sendKeys(arg1);
-		}
-
-		@When("^user can emter the birthYear \"([^\"]*)\"$")
-		public void user_can_emter_the_birthYear(String arg1) throws Throwable {
-			SignupPage.GetYear().sendKeys(arg1);
-		}
-
-		@When("^user can emter the gender \"([^\"]*)\"$")
-		public void user_can_emter_the_gender(String arg1) throws Throwable {
-			SignupPage.GetGender().sendKeys(arg1);
-		}
-
-		@Then("^user can close browser$")
-		public void user_can_close_browser() throws Throwable {
-			driver.manage().deleteAllCookies();	
-			Thread.sleep(1000);
-			obj1.printName();
-			driver.quit();
-		}
-		
-		
-	@Before
-	public void beforeTest() {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\testing drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
+		SignupPage = new YahooSignUpWebobjects(driver);
+		obj1 = new functioncode1();
+		driver.get("https://login.yahoo.com/account/create?specId=yidReg");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);	
-		
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);	
 	}
-		
-		@Given("^the user is on the pendora sign up page$")
-		public void the_user_is_on_the_pendora_sign_up_page() throws Throwable {
-			SP = new PandoraSignUpPageObjects(driver);
-			driver.get("https://www.pandora.com/account/register");
-			
-		}
 
-		@When("^user can validate the URL$")
-		public void user_can_validate_the_URL() throws Throwable {
-			String ActiualUrl = driver.getCurrentUrl();
-			String ExpectedUrl = "https://www.pandora.com/account/register";
-				System.out.println(ActiualUrl);
-			Assert.assertEquals(ActiualUrl, ExpectedUrl);
-			Reporter.log("URL validated");
-		}
+	@Given("^user is able to see correct URl$")
+	public void user_is_able_to_see_correct_URl() throws Throwable {
+		String ActiualUrl = driver.getCurrentUrl();
+		String ExpectedUrl = "https://login.yahoo.com/account/create?specId=yidReg";
+		System.out.println(ActiualUrl);
+		Assert.assertEquals(ActiualUrl, ExpectedUrl);
+		Reporter.log("URL validated");
+	}
 
-		@When("^user can validate the page title$")
-		public void user_can_validate_the_page_title() throws Throwable {
-			String ActualPageTitle = driver.getTitle();
-			String ExpactedPageTitle = "Music and Podcasts, Free and On-Demand | Pandora";
-				System.out.println(ActualPageTitle);
+	@Then("^user can close browser$")
+	public void user_can_close_browser() throws Throwable {
+		driver.manage().deleteAllCookies();	
+		Thread.sleep(3000);
+		obj1.printName();
+		driver.quit();
+	}
 
-			Assert.assertEquals(ActualPageTitle, ExpactedPageTitle);
-			Reporter.log("Page Title validated");
+	@Given("^user can see the correct page title$")
+	public void user_can_see_the_correct_page_title() throws Throwable {
+		String ActualPageTitle = driver.getTitle();
+		String ExpactedPageTitle = "Yahoo";
+		System.out.println(ActualPageTitle);
+		Assert.assertEquals(ActualPageTitle, ExpactedPageTitle);
+		Reporter.log("Page Title validated");
+	}
 
-		}
+	@When("^user can enter the firstname \"([^\"]*)\"$")
+	public void user_can_enter_the_firstname(String arg1) throws Throwable {
+	   SignupPage.UserFirstName().sendKeys(arg1);
+	}
 
-		@When("^user can enter email \"([^\"]*)\"$")
-		public void user_can_enter_email(String arg1) throws Throwable {
-			SP.userEmail().sendKeys(arg1);
-		}
+	@When("^user can enter the lastname \"([^\"]*)\"$")
+	public void user_can_enter_the_lastname(String arg1) throws Throwable {
+		   SignupPage.UserLastName().sendKeys(arg1);
 
-		@When("^user can enter password \"([^\"]*)\"$")
-		public void user_can_enter_password(String arg1) throws Throwable {
-			SP.userPassword().sendKeys(arg1);
-		}
+	}
 
-		@When("^user can enter birthyear \"([^\"]*)\"$")
-		public void user_can_enter_birthyear(String arg1) throws Throwable {
-			SP.userBirthYear().sendKeys(arg1);
-		}
+	@When("^user can enter the email \"([^\"]*)\"$")
+	public void user_can_enter_the_email(String arg1) throws Throwable {
+		   SignupPage.UserEmail().sendKeys(arg1);
+	}
 
-		@When("^user can enter zipcode \"([^\"]*)\"$")
-		public void user_can_enter_zipcode(String arg1) throws Throwable {
-		   SP.userZipCode().sendKeys(arg1);
-		}
+	@When("^user can enter the birth date \"([^\"]*)\"$")
+	public void user_can_enter_the_birth_date(String arg1) throws Throwable {
+	   SignupPage.GetDate().sendKeys(arg1);
+	}
 
-		@Then("^user can exit browser$")
-		public void user_can_exit_browser() throws Throwable {
-			driver.manage().deleteAllCookies();	
-			Thread.sleep(3000);
-			obj1.printName();
-			driver.quit();
-		}
-		
+	@When("^user can enter the birth year \"([^\"]*)\"$")
+	public void user_can_enter_the_birth_year(String arg1) throws Throwable {
+	 SignupPage.GetYear().sendKeys(arg1);
+	}
 	
+	@Given("^user can enter the passward \"([^\"]*)\"$")
+	public void user_can_enter_the_passward(String arg1) throws Throwable {
+		   SignupPage.UserPassward().sendKeys(arg1);
+	}
 
-		
+	@Given("^user can enter the phone \"([^\"]*)\"$")
+	public void user_can_enter_the_phone(String arg1) throws Throwable {
+	  SignupPage.PhoneNumber().sendKeys(arg1);
+	}
 
+	@When("^user can enter the gender \"([^\"]*)\"$")
+	public void user_can_enter_the_gender(String arg1) throws Throwable {
+	   SignupPage.GetGender().sendKeys(arg1);
+	}
+
+	@When("^user can select the birth month \"([^\"]*)\"$")
+	public void user_can_select_the_birth_month(String arg1) throws Throwable {
+		Select Month = new Select(SignupPage.GetMonth());
+		Month.selectByVisibleText("July");
+}
 }
